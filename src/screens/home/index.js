cc1d9b4593764d82138ad8dfd6893acdfcbdb701
Ajay Chawda <ajay@Elvagos-MacBook-Pro.local>
2022-08-14 20:20:56 +0530

    fix: implemented home ui

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -1,9 +1,65 @@
 import React from "react";
 import Carousel from "../../common/component/carousel";
-// import { unisexApi } from "../../firebase/services/snkrs.services";
+import Nike from "../../common/image/nike.png";
+import Jordan from "../../common/image/jordan.png";
+import Adidas from "../../common/image/adidas.png";
+import Reebok from "../../common/image/reebok.png";
+import Puma from "../../common/image/puma.png";
+import Nb from "../../common/image/nb.png";
+import HomeGB from "../../common/image/home-background.jpg";
+
+import "./styles.css";
+
+let data = [Jordan, Nike, Adidas, Reebok, Nb, Puma];
 
 function Home() {
-  return <Carousel />;
+  return (
+    <div id="home-container">
+      <div id="heading">
+        <p className="home-text sub-heading">This is SNKRS</p>
+        <p className="home-text line-header">MADE FOR</p>
+        <p className="home-text line-header">EVERYTING YOU ARE</p>
+        <p id="info-header">
+          The SNKRS collection is designed and tested by top designer. Giving
+          you the freedom to move however you want to, throughout your entire
+          journey.
+        </p>
+      </div>
+
+      {/* <div id="home-background">
+        <img src={HomeGB} width={"100%"} height={"80%"} />
+        <div id="home-shadow"></div>
+      </div> */}
+      <Carousel />
+      <div id="feature">
+        <div class="feature-item">
+          <h1>500+</h1>
+          <p>Shoes</p>
+        </div>
+        <div class="feature-item">
+          <h1>100%</h1>
+          <p>Genuine product</p>
+        </div>
+        <div class="feature-item">
+          <h1>3 Months</h1>
+          <p>Warrenty</p>
+        </div>
+      </div>
+
+      <h3 id="top-brand">Top Brands</h3>
+      <div id="slider">
+        <div class="slide-track">
+          {[...data, ...data].map((image) => {
+            return (
+              <div class="slides">
+                <img src={image} width="250px" height="250px" alt="w" />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Home;
 
