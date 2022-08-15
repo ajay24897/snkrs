c79357f3405ac8f1c91ef20738ed4c187f270547
Ajay Chawda <ajay@Elvagos-MacBook-Pro.local>
2022-08-15 18:57:43 +0530

    feat: checkout

---
+++ src/screens/cart/checkoutModal/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { GrClose } from "react-icons/gr";
+import {} from "../../../common/function";
+import "./styles.css";
+
+function CheckoutModal({ onCancle, data }) {
+  console.log("data?.length", data);
+  return (
+    <div id="checkout-overlay">
+      <div id="checkout-modal-container">
+        <GrClose id="close_icon" onClick={onCancle} />
+        <h3 id="checkout-header">Billing Details</h3>
+        <div id="billing-info-container">
+          <label>Name</label>
+          <input type="text" className="checkout-input" />
+
+          <label>Email</label>
+          <input type="text" className="checkout-input" />
+
+          <label>Phone</label>
+          <input type="text" className="checkout-input" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutModal;
